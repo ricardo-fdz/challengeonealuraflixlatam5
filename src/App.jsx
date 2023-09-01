65d757933815127b0c0537aa17a7ab7f749f6f33
Ricardo Hernandez <hfdz.ricardo@gmail.com>
2023-08-31 18:26:58 -0600

    Project update dependecies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>
-      Aluraflix
-    </h1>
+      <Header/>
+      <Banner/>
+      <Footer/>
     </>
   )
 }
